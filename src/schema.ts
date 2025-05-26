2dffdc371f249960acec07a1a244e7d185eda246
techcoderx <git@techcoderx.com>
2025-05-26 09:52:48 +0800

    reward stats api

--- src/schema.ts
+++ src/schema.ts
@@ -68,7 +68,7 @@ const schema = {
     },
     createFx: async () => {
         await db.client.query(fs.readFileSync(__dirname+'/sql/create_functions.sql','utf-8'))
-        // await db.client.query(fs.readFileSync(__dirname+'/sql/create_apis.sql','utf-8'))
+        await db.client.query(fs.readFileSync(__dirname+'/sql/create_apis.sql','utf-8'))
         logger.info('Created relevant PL/pgSQL functions and types')
     },
     fkExists: async (fk: string) => {
--- src/sql/create_apis.sql
+++ src/sql/create_apis.sql
@@ -7,22 +7,95 @@ GRANT USAGE ON SCHEMA witstats_app TO witstats_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA witstats_api TO witstats_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA witstats_app TO witstats_user;
 
+DROP TYPE IF EXISTS witstats_app.sort_direction CASCADE;
+CREATE TYPE witstats_app.sort_direction AS ENUM (
+  'asc',
+  'desc'
+);
+
+DROP TYPE IF EXISTS witstats_app.granularity CASCADE;
+CREATE TYPE witstats_app.granularity AS ENUM (
+  'daily',
+  'monthly',
+  'yearly'
+);
+
 -- GET /
 CREATE OR REPLACE FUNCTION witstats_api.home()
-RETURNS jsonb
-AS
-$function$
-DECLARE
-    _last_processed_block INTEGER;
-    _db_version INTEGER;
+RETURNS INTEGER AS $function$
 BEGIN
-    SELECT last_processed_block, db_version INTO _last_processed_block, _db_version FROM witstats_app.state;
-    RETURN jsonb_build_object(
-        'last_processed_block', _last_processed_block,
-        'db_version', _db_version
-    );
+	RETURN hive.app_get_current_block_num('witstats_app');
 END
 $function$
 LANGUAGE plpgsql STABLE;
 
--- The rest of PostgREST API methods goes here
+CREATE OR REPLACE FUNCTION witstats_api.witness_reward_stats(producer VARCHAR)
+RETURNS jsonb AS $function$
+DECLARE
+	_producer_id INTEGER;
+BEGIN
+	SELECT id INTO _producer_id FROM hive.irreversible_accounts_view WHERE name = producer;
+	RETURN jsonb_build_object(
+		'total_vests', SUM(reward_vests),
+		'total_hive', SUM(reward_hive),
+		'total_blocks', SUM(block_count)
+	)
+	FROM witstats_app.daily_stats
+	WHERE producer_id = _producer_id;
+END $function$
+LANGUAGE plpgsql STABLE;
+
+CREATE OR REPLACE FUNCTION witstats_api.witness_reward_history(
+  producer VARCHAR,
+  start_date TIMESTAMP = NULL,
+  end_date TIMESTAMP = NULL,
+  direction witstats_app.sort_direction = 'asc',
+  granularity witstats_app.granularity = 'daily'
+)
+RETURNS jsonb AS $function$
+DECLARE
+	_producer_id INTEGER;
+  _start TIMESTAMP = DATE_TRUNC('day', COALESCE(start_date, '1970-01-01'::TIMESTAMP));
+  _end TIMESTAMP = DATE_TRUNC('day', COALESCE(end_date, NOW()::TIMESTAMP));
+BEGIN
+	SELECT id INTO _producer_id FROM hive.irreversible_accounts_view WHERE name = producer;
+	IF granularity = 'daily' THEN
+    RETURN (
+      WITH history AS (
+        SELECT date, reward_vests, reward_hive, block_count
+        FROM witstats_app.daily_stats
+        WHERE producer_id = _producer_id AND date >= _start AND date <= _end
+        ORDER BY
+          (CASE WHEN direction = 'desc' THEN date ELSE NULL END) DESC,
+          (CASE WHEN direction = 'asc' THEN date ELSE NULL END) ASC
+      )
+      SELECT jsonb_agg(jsonb_build_object(
+        'date', date,
+        'vests', reward_vests,
+        'hive', reward_hive,
+        'count', block_count
+      ))
+      FROM history
+    );
+  ELSE
+    RETURN (
+      WITH history AS (
+        SELECT DATE_TRUNC((CASE WHEN granularity = 'monthly' THEN 'month' ELSE 'year' END), date) d, SUM(reward_vests) reward_vests, SUM(reward_hive) reward_hive, SUM(block_count) block_count
+        FROM witstats_app.daily_stats
+        WHERE producer_id = _producer_id AND date >= _start AND date <= _end
+        GROUP BY DATE_TRUNC((CASE WHEN granularity = 'monthly' THEN 'month' ELSE 'year' END), date)
+        ORDER BY
+          (CASE WHEN direction = 'desc' THEN DATE_TRUNC((CASE WHEN granularity = 'monthly' THEN 'month' ELSE 'year' END), date) END) DESC,
+          (CASE WHEN direction = 'asc' THEN DATE_TRUNC((CASE WHEN granularity = 'monthly' THEN 'month' ELSE 'year' END), date) END) ASC
+      )
+      SELECT jsonb_agg(jsonb_build_object(
+        'date', d,
+        'vests', reward_vests,
+        'hive', reward_hive,
+        'count', block_count
+      ))
+      FROM history
+    );
+  END IF;
+END $function$
+LANGUAGE plpgsql STABLE;
